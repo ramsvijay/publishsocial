2ef3047aec06e854ed179b47b99249a0216c10b5
arun <ramsvijay7@gmail.com>
2017-03-21 20:08:12 +0530

    social media

--- src/main/java/com/bean/Status1.java
+++ src/main/java/com/bean/Status1.java
@@ -9,10 +9,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
-import org.junit.Test;
-
 @Entity
 @Table(name = "status1")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
